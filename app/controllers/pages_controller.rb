9db6f8543cca9a53fc4c27e4021d8c5e5510674d
Julius Dewavrin <jdewavrin@gmail.com>
2016-09-06 11:32:41 +0200

    pages skeleton

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def concept
+  end
+
+  def campagne
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  # get 'pages/concept'
+  # get 'pages/campagne'
+  root to: 'pages#home'
+  get 'concept', to: 'pages#concept'
+  get 'campagne', to: 'pages#campagne'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
